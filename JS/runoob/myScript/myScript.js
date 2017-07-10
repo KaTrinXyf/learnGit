2fca007f2f4e40589abe520d734938590674d96d
KaTrinXyf <244022656@qq.com>
2017-07-10 15:22:53 +0800

    first

--- JS/runoob/myScript/myScript.js
+++
@@ -1,8 +0,0 @@
-/*function myFunction()
-{
-	document.getElementById("demo").innerHTMl = ("我的第一个JavaScript函数。");
-}*/
-function myFunction()
-{
-	document.getElementById("demo").innerHTML="我的第一个 JavaScript 函数";
-}
--- NodeJs/安装配置（搭建简易服务器）/server.js
+++
@@ -1,9 +0,0 @@
-var http = require("http");
-
-http.CreateServer(function(request,response)
-{
-	response.writeHead(200,{'content-type':'text/plain'});
-	response.end("Hello World!\n")
-}).listen(8888);
-
-console.log('server running at http://127.0.0.1:8888/');
