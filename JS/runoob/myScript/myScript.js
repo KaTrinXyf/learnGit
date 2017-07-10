43c7f6e562f8d93cc9302c741b2c5563539a9da2
KaTrinXyf <244022656@qq.com>
2017-07-10 15:07:17 +0800

    copy old files

---
+++ JS/runoob/myScript/myScript.js
@@ -0,0 +1,8 @@
+/*function myFunction()
+{
+	document.getElementById("demo").innerHTMl = ("我的第一个JavaScript函数。");
+}*/
+function myFunction()
+{
+	document.getElementById("demo").innerHTML="我的第一个 JavaScript 函数";
+}
