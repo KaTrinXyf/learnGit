a69de0eb2150ad2529bfdc31942d22edda6fcae4
KaTrinXyf <244022656@qq.com>
2017-07-10 15:47:22 +0800

    Original

---
+++ learnGit/VueJs/my-project/src/components/Hello.vue
@@ -0,0 +1,16 @@
+<template>
+	<div class = "hello">
+		<h1>{{ msg }}</h1>
+	</div>
+</template>
+
+<script type="text/javascript">
+	export default {
+		name: 'hello',
+		data() {
+			return {
+				msg: '欢迎来到英雄联盟！'
+			}
+		}
+	}
+</script>
